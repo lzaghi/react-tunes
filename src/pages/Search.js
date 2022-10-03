042ade9628ca6c1c14e260c74bffaed6d41c5655
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-10-03 18:32:31 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isButtonDisabled: true,
+    };
+  }
+
+  handleButton = ({ target }) => {
+    const minimum = 2;
+    if (target.value.length >= minimum) {
+      this.setState({ isButtonDisabled: false });
+    } else {
+      this.setState({ isButtonDisabled: true });
+    }
+  };
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.handleButton }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
