96456c4d9add0f9e753ed319277b656dc20dbebc
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-10-04 10:52:15 -0300

    requisito 7

---
+++ src/components/MusicCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { album } = this.props;
+    return (
+      <div>
+        <p>{album.trackName}</p>
+        <audio data-testid="audio-component" src="{previewUrl}" controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  album: PropTypes.shape({
+    trackName: PropTypes.string,
+  }).isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      musics: [],
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const data = await getMusics(match.params.id);
+    console.log(data);
+    this.setState({ musics: data });
+  }
+
   render() {
+    const { musics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        { musics.length > 0 && (
+          musics.map((album, index) => (
+            index === 0
+              ? (
+                <div key={ index }>
+                  <h3 data-testid="artist-name">{album.artistName}</h3>
+                  <h4 data-testid="album-name">{album.collectionName}</h4>
+
+                </div>)
+              : <MusicCard key={ index } album={ album } />
+          ))
+        )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
