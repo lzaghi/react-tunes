e4b0d3c17895660c4aba8861d6a88dfde4d56c77
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-10-05 10:33:15 -0300

    requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const data = await getUser();
+        this.setState({ loading: false, user: data });
+        console.log(data);
+      },
+    );
+  }
+
   render() {
+    const { loading, user } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { loading
+          ? <Loading />
+          : (
+            <div>
+              <p data-testid="header-user-name">{ user.name }</p>
+              <img data-testid="profile-image" src={ user.image } alt="foto perfil" />
+              <p>{ user.email }</p>
+              <p>{ user.description }</p>
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+          )}
       </div>
     );
   }
