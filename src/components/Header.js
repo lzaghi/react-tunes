245c8783d58a54554c94981d720e859da4217434
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-07-29 16:42:01 -0300

    style: implementa menu expansivo mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
+import styles from './styles/Header.module.css';
+import logo from '../utils/logo-white.png';
 
 class Header extends React.Component {
   constructor() {
@@ -22,22 +24,89 @@ class Header extends React.Component {
     );
   }
 
+  toggleMenu = (status) => {
+    const nav = document.getElementsByClassName(styles.header)[0];
+    nav.classList.toggle(`${styles['menu-expanded']}`, status);
+  };
+
   render() {
     const { loading, nome } = this.state;
 
     return (
-      <>
-        <header data-testid="header-component">
-          { loading
-            ? <Loading />
-            : (
-              <h2 data-testid="header-user-name">{ nome }</h2>
-            )}
+      <nav className={ styles.header }>
+        <header className={ styles['top-bar'] }>
+          <img src={ logo } alt="trybe tunes logo" />
+          <button
+            type="button"
+            className={ styles.open }
+            onClick={ () => this.toggleMenu(true) }
+          >
+            <span className={ `material-symbols-outlined ${styles.icon}` }>
+              menu
+            </span>
+          </button>
+          <button
+            type="button"
+            className={ styles.close }
+            onClick={ () => this.toggleMenu(false) }
+          >
+            <span className={ `material-symbols-outlined ${styles.icon}` }>
+              close
+            </span>
+          </button>
         </header>
-        <Link data-testid="link-to-search" to="/search">Search</Link>
-        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
-        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
-      </>
+
+        <div className={ styles.burger }>
+          <div className={ styles.links }>
+            <div className={ styles.linkBox }>
+              <span className={ `material-symbols-outlined ${styles.iconSm}` }>
+                search
+              </span>
+              <Link
+                data-testid="link-to-search"
+                to="/search"
+                className={ styles.link }
+              >
+                Search
+              </Link>
+            </div>
+            <div className={ styles.linkBox }>
+              <span className={ `material-symbols-outlined ${styles.iconSm}` }>
+                favorite
+              </span>
+              <Link
+                data-testid="link-to-favorites"
+                to="/favorites"
+                className={ styles.link }
+              >
+                Favorites
+              </Link>
+            </div>
+            <div className={ styles.linkBox }>
+              <span className={ `material-symbols-outlined ${styles.iconSm}` }>
+                person
+              </span>
+              <Link
+                data-testid="link-to-profile"
+                to="/profile"
+                className={ styles.link }
+              >
+                Profile
+
+              </Link>
+            </div>
+          </div>
+
+          <div data-testid="header-component" className={ styles.user }>
+            { loading
+              ? <Loading />
+              : (
+                <h2 data-testid="header-user-name">{ nome }</h2>
+              )}
+          </div>
+        </div>
+
+      </nav>
     );
   }
 }
